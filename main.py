3eab66f8e1dfb7c38dbe952564eaa971deb3964b
Imanuel <mrjsaw@gmail.com>
2019-12-15 18:19:18 +0100

    added grid,player,agent,astar

--- main.py
+++ main.py
@@ -1,95 +1,239 @@
 import pygame
 
+# Create a player
+class player():
+    def __init__(self, x, y, orientation):
+        self.x = x
+        self.y = y
+        self.orientation = orientation  # 0=up,1=right,2=down,3=left
+
+class agent():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(maze, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] == 3:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+PINK = (255,153,255)
+
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 40
+HEIGHT = 40
+
+# This sets the margin between each cell
+MARGIN = 5
+
+# Create a 2 dimensional array. A two dimensional
+# array is simply a list of lists.
+grid = []
+for row in range(10):
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    for column in range(10):
+        grid[row].append(0)  # Append a cell
+player = player(1,5,1)
+agent = agent(7,7)
+
+# Set row 1, cell 5 to one. (Remember rows and
+# column numbers start at zero.)
+grid[1][5] = 1
+grid[7][7] = 2
+
+start = (agent.x, agent.y)
+end = (player.x, player.y)
+
+path = astar(grid, start, end)
+print(path)
+# Initialize pygame
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("AI Shooter - Imanuel Vancauteren & Seppe Coninx")
 
-walkRight = pygame.image.load('rightsprite.png')
-walkLeft = pygame.image.load('leftsprite.png')
-walkTop = pygame.image.load('topsprite.png')
-walkBottom = pygame.image.load('bottomsprite.png')
 
+def algo():
+    maze = astar(grid, (agent.x, agent.y), (player.x, player.y))
+    print(maze)
+    grid[agent.x][agent.y] = 0
+    agent.x = maze[1][0]
+    agent.y = maze[1][1]
+    grid[agent.x][agent.y] = 2
+
+
+# Set the HEIGHT and WIDTH of the screen
+WINDOW_SIZE = [450, 450]
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
+# Set title of screen
+pygame.display.set_caption("AI Shooter")
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-class player(object):
-    def __init__(self,x,y,width,height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.speed = 5
-        self.left = False
-        self.right = False
-        self.top = False
-        self.bottom = False
+# -------- Main Program Loop -----------
+while not done:
+    for event in pygame.event.get():  # User did something
+        keys = pygame.key.get_pressed()
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # User clicks the mouse. Get the position
+            pos = pygame.mouse.get_pos()
+            # Change the x/y screen coordinates to grid coordinates
+            column = pos[0] // (WIDTH + MARGIN)
+            row = pos[1] // (HEIGHT + MARGIN)
+            # Set that location to one
+            grid[row][column] = -1
+            print("Click ", pos, "Grid coordinates: ", row, column)
+        elif keys[pygame.K_LEFT] and player.y > 0:
+            grid[player.x][player.y] = 0
+            player.y = player.y - 1
+            grid[player.x][player.y] = 1
+            algo()
 
-    def draw(self, screen):
-        if self.left:
-            screen.blit(walkLeft, (self.x, self.y))
-        elif self.right:
-            screen.blit(walkRight, (self.x, self.y))
-        elif self.top:
-            screen.blit(walkTop, (self.x, self.y))
-        elif self.bottom:
-            screen.blit(walkBottom, (self.x, self.y))
-        else:
-            screen.blit(walkBottom, (self.x, self.y))
+        elif keys[pygame.K_RIGHT] and player.y < 9:
+            grid[player.x][player.y] = 0
+            player.y = player.y + 1
+            grid[player.x][player.y] = 1
+            algo()
+        elif keys[pygame.K_UP] and player.x > 0:
+            grid[player.x][player.y] = 0
+            player.x = player.x - 1
+            grid[player.x][player.y] = 1
+            algo()
+        elif keys[pygame.K_DOWN] and player.x < 9:
+            grid[player.x][player.y] = 0
+            player.x = player.x + 1
+            grid[player.x][player.y] = 1
+            algo()
+    # Set the screen background
+    screen.fill(BLACK)
 
-class projectile(object):
-    def __init__(self,x,y,radius,color,facing):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.color = color
-        self.facing = facing
-        self.speed = 8 * facing
+    # Draw the grid
+    for row in range(10):
+        for column in range(10):
+            color = WHITE
+            if grid[row][column] == 1:
+                color = GREEN
+            elif grid[row][column] == 2:
+               color = RED
+            elif grid[row][column] == -1:
+                color = PINK
+            pygame.draw.rect(screen,
+                             color,
+                             [(MARGIN + WIDTH) * column + MARGIN,
+                              (MARGIN + HEIGHT) * row + MARGIN,
+                              WIDTH,
+                              HEIGHT])
 
-    def draw(self,screen):
-        pygame.draw.circle(screen, self.color, (self.x,self.y), self.radius)
 
-def gameWindow():
-    screen.fill((0, 0, 0))
-    player.draw(screen)
-    pygame.display.update()
-
-
-#Game loop
-player = player(10, 10, 30, 30)
-run = True
-while run:
+    # Limit to 30 frames per second
     clock.tick(30)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
 
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_LEFT] and player.x > player.speed:
-        player.x -= player.speed
-        player.left = True
-        player.right = False
-        player.top = False
-        player.bottom = False
-    elif keys[pygame.K_RIGHT] and player.x < 800 - player.speed - player.width:
-        player.x += player.speed
-        player.left = False
-        player.right = True
-        player.top = False
-        player.bottom = False
-    elif keys[pygame.K_UP] and player.y > player.speed:
-        player.y -= player.speed
-        player.left = False
-        player.right = False
-        player.top = True
-        player.bottom = False
-    elif keys[pygame.K_DOWN] and player.y < 600 - player.height - player.speed:
-        player.y += player.speed
-        player.left = False
-        player.right = False
-        player.top = False
-        player. bottom = True
-
-    gameWindow()
-
+# Be IDLE friendly. If you forget this line, the program will 'hang'
+# on exit.
 pygame.quit()
